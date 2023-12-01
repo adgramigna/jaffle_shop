56b0d032988060d7b0748f4950f3829d54b42484
Andrew Gramigna <andrew.gramigna@brooklyndata.co>
2023-12-01 09:45:07 -0500

    lint initial models

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -9,13 +8,14 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
+with
+    source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+        select 1 as id
+        union all
+        select null as id
 
-)
+    )
 
 select *
 from source_data
