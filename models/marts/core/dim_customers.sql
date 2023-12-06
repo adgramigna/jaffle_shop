53a7b981c91a0f2cdbddee2a40fac48f790d14be
Andrew Gramigna <andrew.gramigna@brooklyndata.co>
2023-12-06 15:45:47 -0500

    finish models part

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,24 +1,13 @@
 with
     customers as (
 
-        select
-            id as customer_id
-            , first_name
-            , last_name
-
-        from raw.jaffle_shop.customers
+        select * from {{ ref('stg_customers') }}
 
     )
 
     , orders as (
 
-        select
-            id as order_id
-            , user_id as customer_id
-            , order_date
-            , status
-
-        from raw.jaffle_shop.orders
+        select * from {{ ref('stg_orders') }}
 
     )
 
@@ -52,6 +41,6 @@ with
 
         left join customer_orders on customers.customer_id = customer_orders.customer_id
 
-    )
+)
 
 select * from final
